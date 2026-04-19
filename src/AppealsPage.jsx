a180fbc988579ea714aaef2d29048bbf0a11b63f
kirbybranden-ui <kirbybranden@gmail.com>
2026-04-19 18:33:47 -0500

    Update AppealsPage.jsx

--- src/AppealsPage.jsx
+++ src/AppealsPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabase";
+import { supabase } from "./lib/supabase";
 
 export default function AppealsPage() {
   const [appeals, setAppeals] = useState([]);
