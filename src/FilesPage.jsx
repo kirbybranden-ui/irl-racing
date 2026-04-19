846ca877eee9e4c1db652dd135e5645586281d3c
kirbybranden-ui <kirbybranden@gmail.com>
2026-04-19 15:34:20 -0500

    Create FilesPage component for displaying documents

--- src/FilesPage.jsx
+++ src/FilesPage.jsx
@@ -1,1 +1,10 @@
-read.me
+import React from "react";
+
+export default function FilesPage() {
+  return (
+    <div style={{ padding: "20px", color: "white" }}>
+      <h1>League Documents</h1>
+      <p>This is where shared files will go.</p>
+    </div>
+  );
+}
