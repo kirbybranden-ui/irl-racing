61e66282bf8e9b5ff25dec951dbcc4bc0a264e25
kirbybranden-ui <kirbybranden@gmail.com>
2026-04-25 09:27:41 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ const defaultDrivers = [
   { id: 5,  number: 3,  name: "ixGusty",                   manufacturer: "Toyota",    team: "Independent" },
   { id: 6,  number: 14, name: "KapSig",                    manufacturer: "Chevrolet", team: "MER"         },
   { id: 7,  number: 24, name: "KEVDINHO7",                 manufacturer: "Chevrolet", team: "MER"         },
-  { id: 8,  number: 38, name: "It's_tricky88",             manufacturer: "Toyota",    team: "Independent" },
+  { id: 8,  number: 38, name: "It's_tricky88",             manufacturer: "Chevrolet", team: "Independent" },
   { id: 9,  number: 97, name: "American_Hero216",          manufacturer: "Ford",      team: "MMS"         },
   { id: 10, number: 67, name: "tallishsinter94",           manufacturer: "Toyota",    team: "Independent" },
   { id: 11, number: 6,  name: "Highlander713",             manufacturer: "Ford",      team: "NLM"         },
