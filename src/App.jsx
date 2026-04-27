1fd2930e660a7d583c45131813b4815f697a31be
kirbybranden-ui <kirbybranden@gmail.com>
2026-04-27 00:13:09 -0500

    Change manufacturer of HOLDEN2DX4EV3R to Toyota

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ const defaultDrivers = [
   { id: 1,  number: 42, name: "AMP-GHOSTRIDER",           manufacturer: "Toyota",    team: "JAM"         },
   { id: 2,  number: 99, name: "RookieVet99",               manufacturer: "Toyota",    team: "JAM"         },
   { id: 3,  number: 18, name: "bowhunter6758",             manufacturer: "Toyota",    team: "JAM"         },
-  { id: 4,  number: 81, name: "HOLDEN2DX4EV3R",            manufacturer: "Chevrolet", team: "JAM"         },
+  { id: 4,  number: 81, name: "HOLDEN2DX4EV3R",            manufacturer: "Toyota",    team: "JAM"         },
   { id: 5,  number: 3,  name: "ixGusty",                   manufacturer: "Toyota",    team: "Independent" },
   { id: 6,  number: 14, name: "KapSig",                    manufacturer: "Chevrolet", team: "MER"         },
   { id: 7,  number: 24, name: "KEVDINHO7",                 manufacturer: "Chevrolet", team: "MER"         },
