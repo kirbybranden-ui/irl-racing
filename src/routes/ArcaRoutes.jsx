8fdaac5525c4fc3cb5ceb2775579a2df0ae21ea7
kirbybranden-ui <kirbybranden@gmail.com>
2026-06-28 22:32:03 -0500

    Add ArcaRoutes component with routing logic

--- src/routes/ArcaRoutes.jsx
+++ src/routes/ArcaRoutes.jsx
@@ -1,1 +1,15 @@
-read.me
+import React from "react";
+import ArcaHomePage from "../series/arca/ArcaHomePage";
+import ArcaStandingsPage from "../series/arca/ArcaStandingsPage";
+import ArcaSchedulePage from "../series/arca/ArcaSchedulePage";
+import ArcaDriversPage from "../series/arca/ArcaDriversPage";
+import ArcaTeamsPage from "../series/arca/ArcaTeamsPage";
+
+export default function ArcaRoutes({ path = "/arca" }) {
+  if (path === "/arca/standings") return <ArcaStandingsPage />;
+  if (path === "/arca/schedule") return <ArcaSchedulePage />;
+  if (path === "/arca/drivers") return <ArcaDriversPage />;
+  if (path === "/arca/teams") return <ArcaTeamsPage />;
+
+  return <ArcaHomePage />;
+}
