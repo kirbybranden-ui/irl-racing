45d1498d52214f8489fede60f8408b84caebaafa
kirbybranden-ui <kirbybranden@gmail.com>
2026-06-28 22:31:41 -0500

    Implement routing logic in AppRoutes component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,1 +1,14 @@
-read.me
+import React from "react";
+import CupRoutes from "./CupRoutes";
+import ArcaRoutes from "./ArcaRoutes";
+
+export default function AppRoutes() {
+  const rawPath = window.location.pathname || "/";
+  const path = rawPath.toLowerCase();
+
+  if (path === "/arca" || path.startsWith("/arca/")) {
+    return <ArcaRoutes path={path} rawPath={rawPath} />;
+  }
+
+  return <CupRoutes path={path} rawPath={rawPath} />;
+}
