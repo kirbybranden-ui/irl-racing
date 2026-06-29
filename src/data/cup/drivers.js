0e3018eca5e75087e31afbda85a2a0ba1ffc1f1c
kirbybranden-ui <kirbybranden@gmail.com>
2026-06-28 21:55:47 -0500

    Add default drivers data to drivers.js

---
+++ src/data/cup/drivers.js
@@ -0,0 +1,23 @@
+export const defaultDrivers = [
+  { id: 2,  number: 99, name: "RookieVet99",               manufacturer: "Toyota",    team: "B2J"         },
+  { id: 3,  number: 18, name: "bowhunter6758",             manufacturer: "Toyota",    team: "B2J"         },
+  { id: 4,  number: 81, name: "HOLDEN2DX4EV3R",            manufacturer: "Toyota",    team: "B2J"         },
+  { id: 6,  number: 14, name: "KapSig",                    manufacturer: "Chevrolet", team: "MER"         },
+  { id: 9,  number: 19, name: "American_Hero216",          manufacturer: "Toyota",    team: "19XI"        },
+  { id: 10, number: 67, name: "tallishsinter94",           manufacturer: "Toyota",    team: "19XI"        },
+  { id: 5,  number: 3,  name: "ixGusty",                   manufacturer: "Toyota",    team: "19XI"        },
+  { id: 34, number: 48, name: "CaJunThrottle28",           manufacturer: "Chevrolet", team: "BXM"         },
+  { id: 54, number: 8,  name: "TheCruiser54",              manufacturer: "Chevrolet", team: "BXM"         },
+  { id: 35, number: 41, name: "KnightTrain41",             manufacturer: "Chevrolet", team: "BXM"         },
+  { id: 102, number: 2, name: "Ghostracer388",             manufacturer: "Chevrolet", team: "BXM"         },
+  { id: 7,  number: 24, name: "KEVDINHO7",                 manufacturer: "Chevrolet", team: "MER"         },
+  { id: 46, number: 39, name: "BigDiehl21",                manufacturer: "Chevrolet", team: "MER"         },
+  { id: 11, number: 6,  name: "Highlander713",             manufacturer: "Ford",      team: "NLM"         },
+  { id: 24, number: 21, name: "kevron-75",                 manufacturer: "Ford",      team: "NLM"         },
+  { id: 23, number: 28, name: "Y2JTolbert",                manufacturer: "Ford",      team: "NLM"         },
+  { id: 18, number: 72, name: "abajack91",                 manufacturer: "Ford",      team: "NLM"         },
+  { id: 26, number: 7,  name: "gunszmb",                   manufacturer: "Ford",      team: "BWR"         },
+  { id: 27, number: 97, name: "JPC_Racing",                manufacturer: "Ford",      team: "BWR"         },
+  { id: 51, number: 51, name: "MARE951",                   manufacturer: "Ford",      team: "BWR"         },
+  { id: 12, number: 23, name: "Orly_Revo23",               manufacturer: "Ford",      team: "MMS",        }
+];
