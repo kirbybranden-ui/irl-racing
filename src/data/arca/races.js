831e0769fc60734f313f1fffe7fdf86892aa283a
kirbybranden-ui <kirbybranden@gmail.com>
2026-06-29 16:41:34 -0500

    Initialize ARCA race schedule with documentation
    Added documentation comment and placeholder for ARCA races.

--- src/data/arca/races.js
+++ src/data/arca/races.js
@@ -1,1 +1,9 @@
-read.me
+/**
+ * ARCA Menards Series - Race Schedule
+ * Define ARCA races here as they're scheduled.
+ */
+
+export const defaultArcaRaces = [
+  // Placeholder - races will be added as season is scheduled
+  // Structure should match: { id, name, track, date, stages, series: "ARCA" }
+];
