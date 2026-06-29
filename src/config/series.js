5f27b952eb825099f4ba7716238e89f13dd3dca9
kirbybranden-ui <kirbybranden@gmail.com>
2026-06-29 15:58:26 -0500

    Add SERIES configuration for racing series

---
+++ src/config/series.js
@@ -0,0 +1,33 @@
+export const SERIES = {
+  cup: {
+    id: "cup",
+    name: "Cup Series",
+    short: "CUP",
+    logo: "/assets/series/NCS.png",
+    color: "#d71920"
+  },
+
+  arca: {
+    id: "arca",
+    name: "ARCA Menards Series",
+    short: "ARCA",
+    logo: "/assets/series/AMS.png",
+    color: "#006341"
+  },
+
+  xfinity: {
+    id: "xfinity",
+    name: "Xfinity Series",
+    short: "NXS",
+    logo: "/assets/series/NXS.png",
+    color: "#0057B8"
+  },
+
+  trucks: {
+    id: "trucks",
+    name: "Craftsman Truck Series",
+    short: "CTS",
+    logo: "/assets/series/CTS.png",
+    color: "#F5A623"
+  }
+};
